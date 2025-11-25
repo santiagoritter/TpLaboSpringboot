b7b5d9b8b466071768ec8c630495d02816d7af39
santiagoritter <santiagoritter26@gmail.com>
2025-11-24 23:06:57 -0300

    Update VeterinariaController.java

--- SpringbootV2.0/src/main/java/com/example/demo/controller/VeterinariaController.java
+++ SpringbootV2.0/src/main/java/com/example/demo/controller/VeterinariaController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class VeterinariaController {
 
     @Autowired private VeterinariaService service;
